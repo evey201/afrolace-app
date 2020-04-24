9f6c193ccfe5dde386432726d67b6706738d0c3f
evey_alabi <evesho.alabi@tropixel.com.ng>
2020-04-24 20:57:03 +0100

    changed firebase key

--- firebase/firebase.utils.js
+++ firebase/firebase.utils.js
@@ -5,14 +5,14 @@ import 'firebase/firebase-firestore';
 
 
 const Config = {
-    apiKey: "AIzaSyAMYCPfOBeG7wfBIdt4fQojUgfXBciG9Aw",
-    authDomain: "afrolace-db.firebaseapp.com",
-    databaseURL: "https://afrolace-db.firebaseio.com",
-    projectId: "afrolace-db",
-    storageBucket: "afrolace-db.appspot.com",
-    messagingSenderId: "23654813638",
-    appId: "1:23654813638:web:3f179ce74a436d4f206cc7",
-    measurementId: "G-R6P9BH4KBB"
+    apiKey: "AIzaSyBUdtGeBrzHMUrxTNWp8iz5m5xWkOwsqVs",
+    authDomain: "afrolace-mobile-app.firebaseapp.com",
+    databaseURL: "https://afrolace-mobile-app.firebaseio.com",
+    projectId: "afrolace-mobile-app",
+    storageBucket: "afrolace-mobile-app.appspot.com",
+    messagingSenderId: "535590365514",
+    appId: "1:535590365514:web:651ec981e22e337d99b9b3",
+    measurementId: "G-KQ2D4BGG7Q"
   };
   // Initialize Firebase
   
